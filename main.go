6b6510095b7d7cc1f60ae0f7dbc9b51fc02d61b7
panhdjf <phuonganh080701a3@gmai.com>
2022-10-12 11:41:15 +0700

    ok

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/panhdjf/scrum/controllers"
 	"github.com/panhdjf/scrum/initializers"
@@ -34,7 +35,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Could not load environment variables", err)
 	}
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"http://localhost:8000", config.ClientOrigin}
+	corsConfig.AllowCredentials = true
 
+	server.Use(cors.New(corsConfig))
 	router := server.Group("/api")
 	router.GET("/healthchecker", func(ctx *gin.Context) {
 		message := "Welcome to Golang with Gorm and Postgres"
